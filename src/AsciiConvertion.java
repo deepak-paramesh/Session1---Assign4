ab6775d5cc681abcedb164298817e5719c58ff70
pade6001 <pade6001@01hw332474.mshome.net>
2017-06-06 19:53:59 +0530

    Session 1 Assign 4

---
+++ src/AsciiConvertion.java
@@ -0,0 +1,17 @@
+
+public class AsciiConvertion {
+
+	public static void main(String[] args) {
+		
+		
+		
+		for(int i=65;i<90;i++)
+		{
+			int integer = i;
+			char character = (char)integer;
+			System.out.println(i + " - " +character);
+		}
+
+	}
+
+}
